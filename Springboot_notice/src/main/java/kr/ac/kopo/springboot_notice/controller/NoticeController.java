710ab46584750de7fe8ab69386df7ff8fc92727d
ohsanghy09 <ohsanghy09@naver.com>
2024-10-16 17:13:45 +0900

    삭제하기 테스트 완료

--- Springboot_notice/src/main/java/kr/ac/kopo/springboot_notice/controller/NoticeController.java
+++ Springboot_notice/src/main/java/kr/ac/kopo/springboot_notice/controller/NoticeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -41,6 +42,23 @@ public class NoticeController {
         // 수정된 메시지를 저장
         NoticeEntity savedMessage = noticeRepository.save(message);
         return ResponseEntity.ok(savedMessage);  // 수정된 데이터를 반환
+    }
+
+    // POST 방식으로 ID를 받아서 해당 데이터를 삭제하는 메서드
+    @PostMapping("/delete")
+    public ResponseEntity<Void> deleteMessageById(@RequestBody Map<String, Long> requestBody) {
+        Long id = requestBody.get("id");  // POST 요청에서 ID 추출
+
+        Optional<NoticeEntity> optionalMessage = noticeRepository.findById(id);
+
+        if (optionalMessage.isPresent()) {
+            // 메시지가 존재할 경우 삭제
+            noticeRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            // 메시지가 존재하지 않을 경우 404 Not Found 반환
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- Vue_notice/src/components/AboutView.vue
+++ Vue_notice/src/components/AboutView.vue
@@ -4,19 +4,29 @@
     <h1>메시지 목록</h1>
     <ul>
       <li v-for="message in messages" :key="message.id">
+        {{ message.id }}<br>
         {{ message.message }}
       </li>
     </ul>
   </div>
 
   <v-btn
-  @click="PostId"
+  @click="PostMessage"
   >보내기</v-btn>
 
   <v-text-field
             v-model="content"
             label="내용"
           ></v-text-field>
+
+          <v-btn
+  @click="DeleteMessage"
+  >삭제하기</v-btn>
+
+  <v-text-field
+            v-model="deleteId"
+            label="아이디 번호"
+          ></v-text-field>       
 </v-container>
 </template>
 
@@ -26,7 +36,8 @@ export default {
   data() {
     return {
       messages: [],  // 서버에서 가져온 메시지 리스트를 저장,
-      content: ''
+      content: '',
+      deleteId: ''
     };
   },
   mounted() {
@@ -38,7 +49,12 @@ export default {
       const response = await this.$axios.get('http://localhost:8080/api/messages/find')
       this.messages = response.data
     },
-    async PostId(){
+
+
+
+
+    
+    async PostMessage(){
       console.log(this.messages[0].id)
       const postId = {
         id : this.messages[0].id,
@@ -47,6 +63,20 @@ export default {
 
       console.log(postId)
       await this.$axios.post("http://localhost:8080/api/messages/update", postId)
+    },
+
+
+
+
+
+
+    async DeleteMessage(){
+      const deleteId = {
+        id : this.deleteId
+      }
+
+      console.log(deleteId)
+      await this.$axios.post("http://localhost:8080/api/messages/delete", deleteId)
     }
 
   }
