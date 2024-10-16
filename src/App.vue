4efc8e44f38ddedade9a8b6de4b06ee448a84455
ohsanghy09 <ohsanghy09@naver.com>
2024-10-16 10:40:27 +0900

    gd

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+      <v-main>
+      <v-container>
+        <div id="app">
+          <router-view></router-view>
+        </div>
+      </v-container>
+</v-main>
+</v-app>
+</template>
+
+  
+
+<script>
+export default {
+  name : 'App',
+};
+</script>
